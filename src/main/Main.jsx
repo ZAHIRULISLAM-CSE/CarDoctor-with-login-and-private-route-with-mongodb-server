a66ea8933f52df1204e2e8b1eccdfdc921221fed
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-10 15:51:18 +0600

    completed the navbar

---
+++ src/main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../pages/home/Home';
+import Nav from '../pages/home/nav/Nav';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/home/nav/Nav.jsx
@@ -0,0 +1,30 @@
+import { Button, Navbar } from "flowbite-react";
+import React from "react";
+import logo from "../../../assets/logo.svg";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <div className="mt-8">
+      <Navbar fluid={true} rounded={true}>
+        <Navbar.Brand href="https://flowbite.com/">
+          <img src={logo} className="mr-3 h-6 sm:h-9" alt="" />
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+          <Button outline={true} gradientDuoTone="pinkToOrange">
+            Appointment
+          </Button>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/services">Services</Link>
+          <Link to="/login">Login</Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Nav;
