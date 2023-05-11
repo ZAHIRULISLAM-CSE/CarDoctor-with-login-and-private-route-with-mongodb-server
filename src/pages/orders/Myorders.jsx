3b983db415c8fdf350a1003640c58942b83b62dd
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-12 01:50:02 +0600

    added deleted option of the order

--- src/pages/orders/Myorders.jsx
+++ src/pages/orders/Myorders.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from 'sweetalert2'
 
 const Myorders = () => {
   const { user, loading } = useContext(AuthContext);
+  
   const [bookingData, setBookingData] = useState([]);
   const email = user?.email;
 
@@ -11,6 +13,28 @@ const Myorders = () => {
       .then((res) => res.json())
       .then((data) => setBookingData(data));
   }, [email]);
+
+
+  const handleDelete=(id)=>{
+
+    fetch(`http://localhost:5000/bookings/${id}`,{
+        method:"DELETE"
+    })
+    .then(res=> res.json())
+    .then(data =>{
+        if(data.deletedCount>0){
+            Swal.fire({
+                title: 'Deleted Successfull',
+                icon: 'success',
+                confirmButtonText:'Okay'
+              })
+        }
+        const remainingBooking=bookingData.filter(booking => booking._id !=id)
+        setBookingData(remainingBooking);
+    });
+
+
+  }
 
   return (
     <div className="mt-12">
@@ -44,7 +68,7 @@ const Myorders = () => {
                   className="bg-white border-b dark:bg-gray-900 dark:border-gray-700"
                 >
                   <th>
-                    <button
+                    <button onClick={()=>handleDelete(booking._id)}
                       type="button"
                       className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500"
                     >
